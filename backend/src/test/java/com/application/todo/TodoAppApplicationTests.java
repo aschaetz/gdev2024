a52f2bc1cdb9a4f0a5c24fc06812e0dd0ac758d3
aschaetz <andreas.schaetzle@genedata.com>
2024-05-03 15:03:45 +0200

    landing demo project

---
+++ backend/src/test/java/com/application/todo/TodoAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.application.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
