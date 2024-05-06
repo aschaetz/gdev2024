65621a5accb1dee23dc007fda8c484b6f8b54a9e
aschaetz <andreas.schasetzle@genedata.com>
2024-05-06 13:35:33 +0000

    add semi-interactive init script

---
+++ backend/init.sh
@@ -0,0 +1,17 @@
+apt update -y && apt install -y sshfs
+mkdir /mnt/pine-data-phymgr/
+sshfs -o allow_other,default_permissions phymgr@pine.ch.genedata.com:/local0/selector/data/dev /mnt/pine-data-phymgr/
+
+# setup GDPROOT remote + local
+mkdir /GDPROOT
+mkdir /GDPROOT/data
+# local content
+# ln -s /IdeaProjects/etc etc
+# remote content
+ln -s /mnt/pine-data-phymgr/projects /GDPROOT/data/projects
+ln -s /mnt/pine-data-phymgr/genomes /GDPROOT/data/genomes
+ln -s /mnt/pine-data-phymgr/agents /GDPROOT/data/agents
+ln -s /mnt/pine-data-phymgr/workflows /GDPROOT/data/workflows
+ln -s /mnt/pine-data-phymgr/nextflow /GDPROOT/data/nextflow
+ln -s /mnt/pine-data-phymgr/indexes/ /GDPROOT/data/indexes
+ln -s /mnt/pine-data-phymgr/activities /GDPROOT/data/activities
