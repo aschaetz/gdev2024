b4dd6c83954866cc8887bc4b68fd1415a17b883d
aschaetz <andreas.schaetzle@genedata.com>
2024-05-08 13:36:52 +0200

    switch back to root based init

--- backend/init.sh
+++ backend/init.sh
@@ -1,16 +1,16 @@
-sudo mkdir -p /mnt/$MOUNT_POINT/
-sudo sshfs -o allow_other,default_permissions phymgr@$REMOTE_SERVER:$REMOTE_FOLDER /mnt/$MOUNT_POINT/
+mkdir -p /mnt/$MOUNT_POINT/
+sshfs -o allow_other,default_permissions phymgr@$REMOTE_SERVER:$REMOTE_FOLDER /mnt/$MOUNT_POINT/
 
 # setup GDPROOT remote + local
-sudo mkdir -p /GDPROOT
-sudo mkdir -p /GDPROOT/data
+mkdir -p /GDPROOT
+mkdir -p /GDPROOT/data
 # local content
 # ln -s /IdeaProjects/etc etc
 # remote content
-sudo ln -s /mnt/$MOUNT_POINT/projects /GDPROOT/data/projects
-sudo ln -s /mnt/$MOUNT_POINT/genomes /GDPROOT/data/genomes
-sudo ln -s /mnt/$MOUNT_POINT/agents /GDPROOT/data/agents
-sudo ln -s /mnt/$MOUNT_POINT/workflows /GDPROOT/data/workflows
-sudo ln -s /mnt/$MOUNT_POINT/nextflow /GDPROOT/data/nextflow
-sudo ln -s /mnt/$MOUNT_POINT/indexes /GDPROOT/data/indexes
-sudo ln -s /mnt/$MOUNT_POINT/activities /GDPROOT/data/activities
+ln -s /mnt/$MOUNT_POINT/projects /GDPROOT/data/projects
+ln -s /mnt/$MOUNT_POINT/genomes /GDPROOT/data/genomes
+ln -s /mnt/$MOUNT_POINT/agents /GDPROOT/data/agents
+ln -s /mnt/$MOUNT_POINT/workflows /GDPROOT/data/workflows
+ln -s /mnt/$MOUNT_POINT/nextflow /GDPROOT/data/nextflow
+ln -s /mnt/$MOUNT_POINT/indexes /GDPROOT/data/indexes
+ln -s /mnt/$MOUNT_POINT/activities /GDPROOT/data/activities
